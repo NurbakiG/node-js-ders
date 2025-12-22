b21ece29d18727bfea4f4d799f8be33ddeb05d4e
NurbakiG <teknoofis42@gmail.com>
2025-12-22 11:57:59 +0300

    routing alt yapısı kuruldu

--- api/app.js
+++ api/app.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV != "production")
-  require('dotenv').config()
+  require('dotenv').config({quiet: true})
 
 
 var createError = require('http-errors');
@@ -7,9 +7,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -23,8 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api',  require('./routes/index'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const fs = require("fs");
+let routes = fs.readdirSync("./routes");
+
+for(let route of routes){
+  if(route.includes(".js") && route != "index.js"){
+    const routePath = "/"+route.replace(".js","");
+    const routeModule = require('./'+route);
+
+    router.use(routePath, routeModule)
+  }
+};
+
 
 module.exports = router;

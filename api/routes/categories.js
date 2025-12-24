fbfcbfc7d1ec6233a419ac7710000ffeb4ccdf84
NurbakiG <teknoofis42@gmail.com>
2025-12-24 21:41:35 +0300

    get,port,update,delete endpointleri yazıldı/ Responseler yazıldı success ve error/ Enumlar yazıldı/  Error(customError yazıldı)

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,18 +1,77 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
+const mongoose = require('mongoose'); 
+const Categories = require('../db/models/Categories');
+const Response = require("../lib/Response");
+const CustomError = require('../lib/Error');
+const Enum = require('../config/Enum');
 
-const isAuthhenticated = true;
-router.all('*',(req,res,next)=>  {
-    if(isAuthhenticated) {
-        next();
-    } else {
-        res.json({success: false, error: "Not Authhenticated"})
+router.get('/', async (req, res) => {
+    try {
+        let categories = await Categories.find({});
+        res.json(Response.successResponse(categories));
+    } catch(err){
+        let errorResponse = Response.errorResponse(err); 
+        res.status(errorResponse.code).json(errorResponse);
     }
-})
-
-router.get('/', (req, res) => {
-  res.json({ success: true });
 });
 
-// DOĞRU EXPORT:
+router.post('/add', async (req, res) => {
+    let body = req.body;    
+    try {        
+        if(!body.name) throw new CustomError(Enum.HTTPS_CODES.BAD_REQUEST, "Validation Error!","name değeri eksik");
+        
+        
+        let category = new Categories({
+            name: body.name,
+            is_active: true,
+            created_by: new mongoose.Types.ObjectId()  // Geçici ObjectId
+        });
+        
+        await category.save();
+        
+        res.json(Response.successResponse({success: true}));
+
+    } catch(err) {
+        let errorResponse = Response.errorResponse(err);        
+        res.status(errorResponse.code).json(errorResponse);
+    }
+});
+
+router.post('/update', async (req, res) => {
+    let body = req.body;    
+    try {        
+        
+        if (!body._id) throw new CustomError(Enum.HTTPS_CODES.BAD_REQUEST, "Validation Error!","id eksik moruk");
+        let update = {};
+
+        if(body.name) update.name = body.name;
+        if(typeof body.is_active === "boolean") update.is_active = body.is_active;
+
+        await Categories.updateOne({_id: body._id}, update)
+        
+        res.json(Response.successResponse({success: true}));
+
+    } catch(err) {
+        let errorResponse = Response.errorResponse(err);        
+        res.status(errorResponse.code).json(errorResponse);
+    }
+});
+
+router.post('/delete', async (req, res) => {
+    let body = req.body;    
+    try {        
+        
+        if (!body._id) throw new CustomError(Enum.HTTPS_CODES.BAD_REQUEST, "Validation Error!","id eksik moruk");
+
+        await Categories.findByIdAndDelete(body._id);
+        
+        res.json(Response.successResponse({success: true}));
+
+    } catch(err) {
+        let errorResponse = Response.errorResponse(err);        
+        res.status(errorResponse.code).json(errorResponse);
+    }
+});
+
 module.exports = router;
